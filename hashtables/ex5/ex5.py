8fd8c480489eab306e4f2fa2507ad6c1a014bda5
Robert Gordon <Komeuppance@gmail.com>
2020-05-08 12:07:49 -0700

    Ex5 success, power is back!

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,9 +1,32 @@
+# from hashtable import HashTable
+
 def finder(files, queries):
-
-    """
-    YOUR CODE HERE
-    """
-
+    result = []
+    cache = {}
+    cache2 = {}
+    for f in files:
+        fsplit = f.split("/")
+        fname = fsplit[-1]
+        # print(fname)
+        if fname not in cache.keys():
+            cache.update({fname: f})
+        elif fname in cache.keys():    
+            # print('duplicate', f)
+            cache2.update({fname: f})
+        # if fname == "file256":
+        #     print('wtf', f)
+        # htable.put(fname, f)
+    for query in queries:
+        # rvalue = htable.get(query)
+        # if rvalue:
+        #     print(rvalue)
+        if query in cache.keys():
+            # print('found', cache[query])
+            result.append(cache[query])
+            if query in cache2.keys():
+                # print('found', cache2[query])
+                result.append(cache2[query])
+    # print(result, len(cache))
     return result
 
 

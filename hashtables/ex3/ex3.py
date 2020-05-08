ceff94914bdb33f966dfab0b326cd4816f664146
Robert Gordon <Komeuppance@gmail.com>
2020-05-08 10:24:04 -0700

    Ex3 success

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,9 +1,36 @@
 def intersection(arrays):
-
-    """
-    YOUR CODE HERE
-    """
-
+    result = []
+    allrays = []
+    cache = {}
+    for n in arrays[0]:
+        cache.update({n: n})
+    for arr in arrays[1:]:
+        for n in arr:
+            if n in cache:
+                # print('intersection found', n)
+                if n not in result:
+                    result.append(n)
+    # for i in range(len(arrays)):
+    #     # print (i)
+    #     allrays.extend(arrays[i])
+    # for n in allrays:
+    #     if n in cache.keys():
+    #         # print('intersection found', n)
+    #         if n not in result:
+    #             result.append(n)
+    #     else:
+    #         cache.update({n: 0})
+    # for arr in arrays:
+    #     # print(len(arr))
+    #     for n in arr:
+    #         if n in cache.keys():
+    #             # print('intersection found', cache[n])
+    #             if n not in result:
+    #                 result.append(n)
+    #             # print(result)
+    #         else:
+    #             cache.update({n: n})
+    #             # print(cache)
     return result
 
 
